76dea53b410e50a17a2e582200e586b73457d37c
Crumpet <anarchysquared2@gmail.com>
2026-03-24 14:18:23 +0000

    Version 0.3

--- Alloy VPN Client.py
+++ Alloy VPN Client.py
@@ -1,91 +1,137 @@
 import subprocess
-from datetime import datetime
 import time
 from pathlib import Path
+from datetime import datetime
 
 BASE_DIR = Path(__file__).parent
-
-LOG_DIR = BASE_DIR / "logs"
+WG_DIR = BASE_DIR / "Wireguard_Configs"
+LOG_DIR = BASE_DIR / "Logs"
+SUB_LOG_DIR = LOG_DIR / str(datetime.today().date())#
 LOG_DIR.mkdir(exist_ok=True)
-path=str(LOG_DIR)
+SUB_LOG_DIR.mkdir(exist_ok=True)
+WG_DIR.mkdir(exist_ok=True)
+log_path=str(SUB_LOG_DIR)
+wg_path=str(WG_DIR)
 
-nowtime=str(datetime.now())
-pre_yap=("####################################################\n### Log Started At: ["+nowtime+"] ###\n####################################################")
-current_time = str(datetime.now().strftime("%Y%m%d%H%M%S"))
-
-pre_filename=[path+"/log", current_time, ".txt"]
-filename="".join(pre_filename)
-f = open(filename, "x")
-f.write(str(pre_yap))
-f.close()
+now=time.strftime("%Y-%m-%d %H:%M:%S")
+log_header=("####################################################\n### Log Started At: ["+now+"] ###\n####################################################")
+current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+filename = log_path + "/log" + current_time + ".txt"
+with open(filename, "x") as f:
+    f.write(log_header)
 print(filename)
 
-def run_command(command):
+def run_command(command): #Call to run command and give output
     output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, shell=True)
     output = str(output.stdout)
     write_to_log(command, output)
     return output
 
-def write_to_log(command, output):
+def run_command_lines(command): #Only used in admin version to get the interface name for the wireguard subroutine but it's usefull to have anyway in case future expansions need to parse other outputs
+    output = run_command(command)
+    return output.splitlines()
+
+def write_to_log(command, output): #Writes to a log file
     global filename
     current_time = str(datetime.now())
-    pretty_time=str("["+current_time+"]: ")
+    pretty_time="["+current_time+"]: "
     pretty_command_list=["Command: ", pretty_time, command]
     pretty_output_list=["Output:", pretty_time, output]
-    pretty_command=str(" ".join(pretty_command_list))
-    pretty_output=str(" ".join(pretty_output_list))
-    f = open(filename, 'a')
-    f.write("\n"+pretty_command)
-    f.write("\n"+pretty_output)
-    f.close()
+    pretty_command= " ".join(pretty_command_list)
+    pretty_output= " ".join(pretty_output_list)
+    with open(filename, 'a') as f:
+        f.write("\n"+pretty_command)
+        f.write("\n"+pretty_output)
 
-def ts_options():
+def ts_options(): #Tailscale VPN
     output=run_command("tailscale status")
-    if "100" in output:
-        print("Tailscale is running.\n")
-        ts_decision=input("Tailscale is Connected, Do you want to disconnect?(y/n)")
-        if ts_decision == ("y"):
-            output=run_command("tailscale down")
-        elif ts_decision == ("n"):
-            pass
+    while True:
+        if "100." in output:
+            decision=input("Tailscale is Connected, Do you want to disconnect?(y/n)")
+            if decision == ("y"):
+                output=run_command("tailscale down")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+        elif "stopped" in output:
+            decision=input("Tailscale is Disconnected, Do you want to connect?(y/n)")
+            if decision == ("y"):
+                output=run_command("tailscale up")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+                break
         else:
-            print("That wasn't an option, please use y/n")
-            ts_options()
-    if "stopped" in output:
-        ts_decision=input("Tailscale is Disconnected, Do you want to connect?(y/n)")
-        if ts_decision == ("y"):
-            output=run_command("tailscale up")
-        elif ts_decision == ("n"):
-            pass
-        else:
-            print("That wasn't an option, please use y/n")
-            ts_options()
+            break
 
-def cf_options():
+def cf_options(): #Cloudflare Warp VPN
     output=run_command("warp-cli status")
-    if "Connected" in output:
-        cf_decision=input("Warp is Connected, Do you want to disconnect?(y/n)")
-        if cf_decision == ("y"):
-            output=run_command("warp-cli disconnect")
-        elif cf_decision == ("n"):
-            pass
-        else:
-            print("That wasn't an option, please use y/n")
-            cf_options()
-    if "Disconnected" in output:
-        cf_decision=input("Warp is Disconnected, Do you want to connect?(y/n)")
-        if cf_decision == ("y"):
-            output=run_command("warp-cli connect")
-        elif cf_decision == ("n"):
-            pass
-        else:
-            print("That wasn't an option, please use y/n")
-            cf_options()
-    if "Connecting" in output:
-        print("Warp is currently connecting, please wait and we will reload...")
-        time.sleep(10)
+    while True:
+        if "Connected" in output:
+            decision=input("Warp is Connected, Do you want to disconnect?(y/n)")
+            if decision == ("y"):
+                output=run_command("warp-cli disconnect")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+        if "Disconnected" in output:
+            decision=input("Warp is Disconnected, Do you want to connect?(y/n)")
+            if decision == ("y"):
+                output=run_command("warp-cli connect")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+        if "Connecting" in output:
+            print("Warp is currently connecting, please wait...")
+            while "Connecting" in output:
+                time.sleep(3)
+                print("Still connecting, retrying in 3 seconds...")
+                output=run_command("warp-cli status")
+            break
 
-def prechecks():
+def mv_options(): #Mullvad VPN
+    output=run_command("mullvad status")
+    while True:
+        if "Connected" in output:
+            decision=input("Mullvad is Connected, Do you want to disconnect?(y/n)")
+            if decision == ("y"):
+                output=run_command("mullvad disconnect")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+        if "Disconnected" in output:
+            decision=input("Mullvad is Disconnected, Do you want to connect?(y/n)")
+            if decision == ("y"):
+                output=run_command("mullvad connect")
+                break
+            elif decision == ("n"):
+                break
+            else:
+                print("That wasn't an option, please use y/n")
+        if "Connecting" in output:
+            print("Mullvad is currently connecting, please wait...")
+            while "Connecting" in output:
+                time.sleep(3)
+                print("Still connecting, retrying in 3 seconds...")
+                output=run_command("mullvad status")
+            break
+
+def prechecks(): #Runs checks before the options menu
+    output=run_command("tailscale status")
+    if "stopped" in output:
+        print("Tailscale Is Disconnected")
+    elif "100." in output:
+        print("Tailscale Is Connected")
     output=run_command("warp-cli status")
     if "Connected" in output:
         print("Cloudflare Warp Is Connected")
@@ -93,25 +139,30 @@ def prechecks():
         print("Cloudflare Warp is Disonnected")
     elif "Connecting" in output:
         print("Warp is currently connecting")
-    output=run_command("tailscale status")
-    if "stopped" in output:
-        print("Tailscale Is Disconnected")
-    elif "100" in output:
-        print("Tailscale Is Connected")
+    output=run_command("mullvad status")
+    if "Connected" in output:
+        print("Mullvad Is Connected")
+    elif "Disconnected" in output:
+        print("Mullvad is Disonnected")
+    elif "Connecting" in output:
+        print("Mullvad is currently connecting")
 
 def options():
     while True:
         prechecks()
         print("1: Tailscale")
         print("2: Cloudflare Warp")
-        print("3: Exit")
+        print("3: Mullvad")
+        print("4: Exit")
         option=input("Please select an option:")
         if option == ("1"):
             ts_options()
         elif option == ("2"):
             cf_options()
         elif option == ("3"):
+            mv_options()
+        elif option == ("4"):
             exit()
         else:
-            print("That wasn't an option, please select 1, 2, or 3")
+            print("That wasn't an option, please select 1, 2, 3, or 4")
 options()
